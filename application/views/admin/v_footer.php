a5f9da2bb8d048a82613c389837a1579490986e8
luluu <lulu.agustin105319@students.unila.ac.id>
2022-01-19 14:44:36 +0700

    First Commit

---
+++ application/views/admin/v_footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.0
+    </div>
+    <strong>Copyright &copy;  <?php echo date("Y") ?> Bhinneka Software | Made with <span i class="fa fa-heart" style="color:#DC3545"> </span> by <a href="https://blogbangirfan.blogspot.com" target="_blank">Ahmad Irfan</a> in Bhinneka Software.</strong> | Distributed by <a href="https://blogbangirfan.blogspot.com" target="_blank" rel="noopener noreferrer">BlogBangIrfan</a>
+  </footer>
